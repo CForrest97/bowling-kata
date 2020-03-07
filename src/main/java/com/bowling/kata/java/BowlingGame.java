7fdd8e9907388d815f63401eb46594a84659292f
CForrest97 <craforrest97@gmail.com>
2020-03-07 10:19:34 +0000

    add test 2
    Signed-off-by: CForrest97 <craforrest97@gmail.com>

--- src/main/java/com/bowling/kata/java/BowlingGame.java
+++ src/main/java/com/bowling/kata/java/BowlingGame.java
@@ -1,10 +1,11 @@
 package com.bowling.kata.java;
 
 public class BowlingGame {
+  private int score = 0;
   public void roll(int pins) {
-    
+    score += pins;
   }
   public int getScore() {
-    return 0;
+    return score;
   }
 }
--- src/test/java/com/bowling/kata/java/BowlingGameTest.java
+++ src/test/java/com/bowling/kata/java/BowlingGameTest.java
@@ -2,15 +2,32 @@ package com.bowling.kata.java;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
+  private BowlingGame g;
+
+  @Before
+  public void setUp() {
+    g = new BowlingGame();
+  }
+
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      g.roll(pins);
+    }
+  }
+  
   @Test
   public void testGutterGame() {
-    BowlingGame g = new BowlingGame();
-    for (int i = 0; i < 20; i++) {
-      g.roll(0);
-    }
+    rollMany(20, 0);
     assertEquals(g.getScore(), 0);
+  }
+  
+  @Test
+  public void testAllOnes() {
+    rollMany(20, 1);
+    assertEquals(g.getScore(), 20);
   }
 }
